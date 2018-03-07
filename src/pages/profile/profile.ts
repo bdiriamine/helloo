c9ec241e2426612a2980c2712effd7c7ce3cd692
bdiriamine <32433979+bdiriamine@users.noreply.github.com>
2018-03-07 01:32:08 +0100

    welcome8.ts

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController,App, NavParams } from 'ionic-angular';
 import { Profile } from '../../model/profile';
 import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
 import {AngularFireDatabase} from 'angularfire2/database';
 import { HomePage } from '../home/home';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { WelcomePage } from '../welcome/welcome';
  
 /**
  * Generated class for the ProfilePage page.
@@ -24,20 +27,24 @@ export class ProfilePage {
     photoURL :'',
     logedin : false
   };
+  
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
 profile = {} as Profile ; 
-  constructor(private auth :AngularFireAuth,private authdata:AngularFireDatabase , public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private auth :AngularFireAuth,private authdata:AngularFireDatabase , public navCtrl: NavController, public navParams: NavParams,public app: App) {
   this.getinfo.displayName = this.navParams.get('name');
   this.getinfo.email = this.navParams.get('email');
   this.getinfo.photoURL = this.navParams.get('photoURL');
-  this.getinfo.logedin = this.navParams.get('logedin');
+  this.getinfo.logedin = this.navParams.get('bolez');
   }
 
   ionViewDidLoad() {
     console.log(this.getinfo.displayName + " " + this.getinfo.email + " " + this.getinfo.photoURL + " " +this.getinfo.logedin);
   }
-creatprofile(){
-  this.auth.authState.take(1).subscribe(auth =>{
-    this.authdata.list(`profile/${auth.uid}`).push(this.profile).then(() => this.navCtrl.push(HomePage));
-  })
+
+logout(){
+  // Remove API token 
+  this.app.getRootNav().setRoot(WelcomePage);
+  
 }
 }
